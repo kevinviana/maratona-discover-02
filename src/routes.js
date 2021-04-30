f82e6c25956440ee3078455fcf33008eccfe4a50
Kevin Viana Pereira <vianakevinc2@hotmail.com>
2021-04-30 19:28:18 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const routes = express.Router()
 
-const basePath = __dirname + '/views'
-
-routes.get('/', (req, res) => res.render(basePath+'/index.html'));
-routes.get('/job', (req, res) => res.render(basePath+'/job.html'));
-routes.get('/job/edit', (req, res) => res.render(basePath+'/job-edit.html'));
-routes.get('/profile', (req, res) => res.render(basePath+'/profile.html'));
+routes.get('/', (req, res) => res.render('/index.html'));
+routes.get('/job', (req, res) => res.render('/job.html'));
+routes.get('/job/edit', (req, res) => res.render('/job-edit.html'));
+routes.get('/profile', (req, res) => res.render('/profile.html'));
 
 
 
