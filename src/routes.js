ce9ab075feef0c1155e22cbec58b5965a7fcb2a2
Kevin Viana Pereira <vianakevinc2@hotmail.com>
2021-05-14 03:11:05 -0300

    :arrows_counterclockwise: update job/delete

--- src/routes.js
+++ src/routes.js
@@ -139,6 +139,14 @@ const Job = {
                 return job
             })
             res.redirect('/job/' + jobId)
+        },
+        delete(req,res) {
+
+            const jobId = req.params.id
+
+            Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId))
+
+            return res.redirect('/')
         }
     },
 
@@ -171,6 +179,7 @@ routes.get('/job', Job.controllers.create)
 routes.post('/job', Job.controllers.save)
 routes.get('/job/:id', Job.controllers.show)
 routes.post('/job/:id', Job.controllers.update)
+routes.post('/job/delete/:id', Job.controllers.delete)
 routes.get('/profile', Profile.controllers.index)
 routes.post('/profile', Profile.controllers.update)
 
