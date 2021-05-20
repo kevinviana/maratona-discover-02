7ea592e6e901d52e3d12a88126abc1e11db90b9e
Kevin Viana Pereira <vianakevinc2@hotmail.com>
2021-05-20 02:41:36 -0300

    :factory: refactor DashboardController

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -2,10 +2,18 @@ const Job = require('../models/Job');
 const Profile = require('../models/Profile');
 const JobUtils = require('../utils/JobUtils');
 
+
 module.exports = {
     index(req, res) {
         const jobs = Job.get();
         const profile = Profile.get();
+
+        let statusCount = {
+            progress: 0,
+            done: 0,
+            total: jobs.length
+        }
+        let jobsInProgressTotalHours = 0;
 
         const updatedJobs = jobs.map((job) => {
 
@@ -13,13 +21,20 @@ module.exports = {
 
             const status = remaining <= 0 ? 'done' : 'progress'
 
+            statusCount[status] += 1;
+
+            jobsInProgressTotalHours =  status == 'progress' ? jobsInProgressTotalHours + Number(job["daily-hours"]) : jobsInProgressTotalHours
+
             return {
                 ...job,
                 remaining,
                 status,
                 budget: JobUtils.calculateBudget(job, profile["value-hour"])
             }
-        })
-        return res.render("index", { jobs: updatedJobs })
+        });
+        // qtde de horas que quero trabalhar dia - quantidade de horas dia dos      jobs.status = progress
+        const freeHours = profile["hours-per-day"] - jobsInProgressTotalHours;
+
+        return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours })
     }
 }
