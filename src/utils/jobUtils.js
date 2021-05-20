d86958099212b2609fb9edaf9a8703317345ad64
Kevin Viana Pereira <vianakevinc2@hotmail.com>
2021-05-20 01:19:56 -0300

    :factory: refactor Jobs

---
+++ src/utils/jobUtils.js
@@ -0,0 +1,22 @@
+module.exports = {
+    remainingDays(job) {
+
+        const daysToDueDate = Math.ceil(job["total-hours"] / job["daily-hours"])
+
+        const createdDate = new Date(job.created_at)
+        const dueDay = createdDate.getDate() + Number(daysToDueDate)
+        const dueDateInMs = createdDate.setDate(dueDay)
+
+        const remainingDaysInMs = dueDateInMs - Date.now()
+        const oneDayInMs = 1000 * 60 * 60 * 24
+
+        const remainingDaysInDays = Math.ceil(remainingDaysInMs / oneDayInMs)
+
+        return remainingDaysInDays
+    },
+    calculateBudget(job, valueHour) {
+        const budget = valueHour * job['total-hours']
+
+        return budget
+    }
+}
