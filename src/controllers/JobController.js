63b79d1a4c40dc8422950724f176902143e04adf
Kevin Viana Pereira <vianakevinc2@hotmail.com>
2021-05-26 14:47:54 -0300

    add db delete & update commands

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -21,52 +21,37 @@ module.exports = {
         const jobs = await Job.get();
         const profile = await Profile.get();
 
-        const jobId = req.params.id
+        const jobId = req.params.id;
 
         const job = jobs.find(job => Number(job.id) === Number(jobId))
 
         if (!job) {
-            return res.send('Job not found')
+            return res.send('Job not found');
         }
 
-        job.budget = JobUtils.calculateBudget(job, profile["value-hour"])
-        return res.render("job-edit", { job })
+        job.budget = JobUtils.calculateBudget(job, profile["value-hour"]);
+        return res.render("job-edit", { job });
     },
 
     async update(req, res) {
-        const jobId = req.params.id
-        const jobs = await Job.get();
-
-        const job = jobs.find(job => Number(job.id) === Number(jobId))
-
-        if (!job) {
-            return res.send('Job not found')
-        }
-
+        const jobId = req.params.id;
+        
         const updatedJob = {
-            ...job,
             name: req.body.name,
             "total-hours": req.body["total-hours"],
             "daily-hours": req.body["daily-hours"]
-        }
+        };
 
-        const newJobs = jobs.map((job) => {
+        await Job.update(updatedJob, jobId);
 
-            if(Number(job.id) === Number(jobId)) {
-                job = updatedJob
-            }
-            return job
-        });
-        Job.update(newJobs)
-
-        res.redirect('/job/' + jobId)
+        res.redirect('/job/' + jobId);
     },
 
-    delete(req,res) {
-        const jobId = req.params.id
+    async delete(req,res) {
+        const jobId = req.params.id;
         
-        Job.delete(jobId)
+        await Job.delete(jobId);
 
-        return res.redirect('/')
+        return res.redirect('/');
     }
 }
